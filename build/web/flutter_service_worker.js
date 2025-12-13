8543d3d92a2f88070ed11a00d8708054391f9f9e
henrymusonda577-sketch <henrymusonda577@gmail.com>
2025-12-14 01:00:35 +0200

    Add compiled Flutter web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -44,7 +44,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4ecbb2e4f54cb46597e2f428fa737365
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "5f5da99a1487d7fbc662333e2a68f775",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "352a0583aa019a9c2b925ad34e7ed25f",
+"flutter_bootstrap.js": "6465e5ba4a452db2db24e709ebad6593",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
